8a2d3d4859a0bfa9dc2119872fc3cf792fe2f0e0
Aldo Hernández <aldo.hernandez@bicg.com>
2024-01-23 00:58:32 -0600

    adding Screen Container

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -10,12 +10,18 @@ interface Props {
 export const ScreenContainer = ({children, title, subtitle}: Props) => {
   return (
     <>
-      <SafeAreaView style={{padding: 10}}>
+      <SafeAreaView style={{paddingHorizontal: 15, paddingVertical: 10}}>
         <View style={{marginTop: 10}}>
-          <Text id="title">{title}</Text>
+          <Text
+            id="title"
+            style={{fontSize: 32, color: '#000', fontWeight: 'bold'}}>
+            {title}
+          </Text>
         </View>
         <View style={{marginTop: 5}}>
-          <Text id="subtitle">{subtitle}</Text>
+          <Text id="subtitle" style={{fontSize: 18, color: '#999'}}>
+            {subtitle}
+          </Text>
         </View>
         <View style={{marginTop: 5}}>{children}</View>
       </SafeAreaView>
